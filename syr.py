cd710d5c9b55dfa51992fc3c1590cb9e96987b04
aqeel <aqeel.ahamad@gmail.com>
2013-09-16 23:59:18 +0100

    s

--- syr.py
+++ syr.py
@@ -1,4 +1,6 @@
-import os 
+import sys
+import re
+import os
 import requests
 import jinja2
 from requests_oauthlib import OAuth1
@@ -6,10 +8,10 @@ from flask import render_template
 from flask import Flask
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-# ...
+
+
+
 app = Flask('myapp', template_folder=tmpl_dir)
-
-
 app = Flask(__name__)
 
 BASE_URL = u'https://api.twitter.com/1.1/'
@@ -38,7 +40,7 @@ bodyoauth = OAuth1(client_key, client_secret,
                    
 @app.route('/')			   
 def sy():
-	payload = {'q': 'syawal','count':5}
+	payload = {'q': 'syria','count':5}
 	url = 'https://api.twitter.com/1.1/search/tweets.json'
 	r = requests.get(url, auth=oauth,params=payload)
 	a=r.json()
@@ -48,10 +50,11 @@ def sy():
 		st=a['statuses'][i]['text']
 		t=time.encode('utf8')
 		s=st.encode('utf8')
-		tup=[s,t]
+		tup=(s,t)
 		b.append(tup)
 	return render_template('syr.html',updates=b)
 
-
+if __name__ == "__main__":
+    app.run()
 
     
