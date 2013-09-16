0292e1c074c44cf55752f0fb685d1edadc9f64f2
aqeel <aqeel.ahamad@gmail.com>
2013-09-17 00:24:23 +0100

    ars

--- syr.py
+++ syr.py
@@ -48,14 +48,14 @@ def sy():
 	for i in range(payload['count']):
 		time=a['statuses'][i]['created_at']
 		st=a['statuses'][i]['text']
-		t=time.encode('utf8')
-		s=st.encode('utf8')
-		tup=(i,s,t)
+		#t=time.encode('utf8')
+		#s=st.encode('utf8')
+		tup=(st,time)
 		b.append(tup)
 	#return render_template('syr.html',updates=b)
 	return b
 
 #if __name__ == "__main__":
 #    app.run()
-print sy()
+#print sy()
     
