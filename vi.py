bc727b43a2295ad207a8d271b0dcf68d8e393aab
aqeel <aqeel.ahamad@gmail.com>
2013-10-01 02:15:48 +0100

    value

--- vi.py
+++ vi.py
@@ -57,8 +57,8 @@ def vi():
 		s=st.encode('utf8')
 		tup=(st,time)
 		b.append(tup)
-	#return render_template('vi.html',updates=b)
-	return b
+	return render_template('vi.html',updates=b)
+	#return b
 
 
 @app.route('/about')
